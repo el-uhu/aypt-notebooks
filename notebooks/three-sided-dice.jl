2b28d00432d12ab27ef444afcdc61cd53d93ee3b
Lukas Hutter <luki@biotop.co>
2021-10-03 15:10:21 +0200

    setup repo

---
+++ notebooks/three-sided-dice.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 93de47da-244a-11ec-2f0a-5d3c11819d2b
+md"""
+# Three-Sided Dice
+
+## Aufgabe
+
+*To land a coin on its side is often associated with the idea of a rare occurrence. What should be the physical and geometrical characteristics of a cylindrical dice so that it has the same probability to land on its side and one of its faces?*
+
+Münzen landen sehr selten auf der Kante, weil die Kante viel schmäler ist als die beiden Seiten. Wie müsste eine zylindrische “Münze”/”Würfel” aussehen umgenauso wahrscheinlich auf der “Kante” zu landen wie auf den Seiten?
+
+## Mögliche Fragestellungen
+
+- Wie sollte die Form so einer fairen 3-seitigen “Münze” aussehen? Wie dick muss sie relativ zum Durchmesser sein?
+- Ist das Material relevant? Ist die perfekte Form für eine harte Münze anders als für eine weiche (z.B. aus Gummi oder Schaumstoff)? Ist der Untergrund auf den man diese “Münze” wirft relevant?
+- Ist die Art und Weise wie man die “Münze” wirft relevant?
+
+## Experimente
+
+### Dicke der Münze
+
+Der wahrscheinlich wichtigste Parameter ist die Dicke der Münze. Sehr schmale Münzen werden fast immer auf einer der beiden Seiten landen. Sehr dicke “Münzen”/Zylinder fast immer auf der “Kante”. Bau dir verschieden dicke Münzen und wirf jede davon eine gegebene Anzahl (z.B. je 20 Mal). Wie viel % der Würfe landen jeweils auf der Kante? Wie sieht die Wahrscheinlichkeit auf der Kante zu landen als Funktion der Dicke aus? Eine mögliche Darstellung könnte eine Grafik ähnlich zu dieser hier sein:
+"""
+
+# ╔═╡ Cell order:
+# ╠═93de47da-244a-11ec-2f0a-5d3c11819d2b
---
+++ src/AYPTNotebooks.jl
@@ -0,0 +1,5 @@
+module AYPTNotebooks
+
+greet() = print("Hello World!")
+
+end # module
